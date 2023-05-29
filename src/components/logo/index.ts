d501aaa28a2b39c712e0371a4f58217ae8715479
Danilo Rodrigues <danilo@fillet.com.br>
2023-05-29 12:08:02 -0300

    Refactoring

--- src/components/logo/index.ts
+++ src/components/logo/index.ts
@@ -1,7 +1,7 @@
 import chalk from 'chalk';
 import figlet from 'figlet';
 
-import { print } from '../../utils/screen';
+import { print } from '../../utils/Screen';
 
 const Logo = {
   show: () => print(chalk.yellow(figlet.textSync('Fillet'))),
