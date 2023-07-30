4b9c939e4b74ddd384d754ccbc815a8bc0ee6ad0
Danilo Rodrigues <danilo@fillet.com.br>
2023-07-29 21:46:11 -0300

    Rollback the chalk versions

--- src/components/message/index.ts
+++ src/components/message/index.ts
@@ -1,7 +1,7 @@
-import chalk, { ColorName } from 'chalk';
+import chalk from 'chalk';
 
 import { print } from '../../utils/Screen';
 
-const message = (value: string, color: ColorName = 'white') => print(chalk[color](value));
+const message = (value: string, color: typeof chalk.Color = 'white') => print(chalk[color](value));
 
 export default message;
