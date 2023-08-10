2d1fa60a4dbc7d11b4feb5fabbca524d878180be
Tiago Bega <tiago@fillet.com.br>
2023-08-10 17:19:02 -0300

    removing typeRoots and changing the name of the types folder to @types

--- app-base/src/config/App.ts
+++ app-base/src/config/App.ts
@@ -1,27 +1,34 @@
-import type { TargetEnvConfig, AppConfig } from '../types/Config';
+import type { TargetEnvConfig, AppConfig } from "../@types/Config";
 
-const env = process.env.APP_ENV || 'development';
-const host = process.env.HOST || 'localhost';
+const env = process.env.APP_ENV || "development";
+const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 4000;
 
-const pageTitle = 'Fillet.JS';
-const cookieName = 'app';
+const pageTitle = "Fillet.JS";
+const cookieName = "app";
 
 const config: TargetEnvConfig = {
+  /**
+   * example: {
+   *  path: "/example/", // This param should start and end with "/"
+   *  base: "http://base.com.br", // This param shouldn't have "/" at the end
+   *  endpoint: "https://api.com.br/api/v1",
+   * }
+   */
   development: {
-    path: '/',
+    path: "/",
     baseUrl: `http://${host}:${port}`,
-    endpoint: '',
+    endpoint: "",
   },
   uat: {
-    path: '/',
-    baseUrl: '',
-    endpoint: '',
+    path: "/teste-build/",
+    baseUrl: "https://zone132.fillet-digital.com.br",
+    endpoint: "",
   },
   production: {
-    path: '/',
-    baseUrl: '',
-    endpoint: '',
+    path: "/",
+    baseUrl: "",
+    endpoint: "",
   },
 };
 
