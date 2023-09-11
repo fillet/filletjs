98e96e16dee3392942a426d52501d5fd94b4f4ec
Luis  Bovo <luis@fillet.com.br>
2023-09-11 14:50:47 -0300

    Moved App.ts to the Webpack folder and configured in DefinePlugin, preventing the exposure of sensitive data from other environments in the final build of the application.

---
+++ app-base/webpack/App.ts
@@ -0,0 +1,41 @@
+import type { TargetEnvConfig, AppConfig } from '../src/@types/Config';
+
+const env = process.env.APP_ENV || 'development';
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 4000;
+
+const pageTitle = 'Fillet.JS';
+const cookieName = 'app';
+
+const config: TargetEnvConfig = {
+  /**
+   * example: {
+   *  path: "/example/", // This param should start and end with "/"
+   *  base: "http://base.com.br", // This param shouldn't have "/" at the end
+   *  endpoint: "https://api.com.br/api/v1",
+   * }
+   */
+  development: {
+    path: '/',
+    baseUrl: `http://${host}:${port}`,
+    endpoint: '',
+  },
+  uat: {
+    path: '/',
+    baseUrl: '',
+    endpoint: '',
+  },
+  production: {
+    path: '/',
+    baseUrl: '',
+    endpoint: '',
+  },
+};
+
+export default {
+  ...config[env],
+  env,
+  pageTitle,
+  cookieName,
+  port,
+} as AppConfig;
--- app-base/webpack/Production.ts
+++ app-base/webpack/Production.ts
@@ -1,61 +1,61 @@
-import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 //PLUGINS
-import CopyPlugin from "copy-webpack-plugin";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import * as path from "path";
-import { Configuration } from "webpack";
-import { merge } from "webpack-merge";
+import CopyPlugin from 'copy-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import * as path from 'path';
+import { Configuration } from 'webpack';
+import { merge } from 'webpack-merge';
 
-import appConfig from "../src/config/App";
-import common from "./Common";
+import appConfig from './App';
+import common from './Common';
 
 const plugins = [
   new HtmlWebpackPlugin({
     title: appConfig.pageTitle,
-    inject: "body",
+    inject: 'body',
     hash: true,
     minify: true,
-    scriptLoading: "defer",
-    filename: "index.html",
-    template: "src/views/index.html",
+    scriptLoading: 'defer',
+    filename: 'index.html',
+    template: 'src/views/index.html',
   }),
   new CleanWebpackPlugin(),
   new MiniCssExtractPlugin({
-    filename: "assets/main-[name]-[chunkhash].css",
+    filename: 'assets/main-[name]-[chunkhash].css',
   }),
   new CopyPlugin({
-    patterns: [{ from: "public", to: "public" }],
+    patterns: [{ from: 'public', to: 'public' }],
   }),
 ];
 
 const prodConfig: Configuration = merge(common, {
-  mode: "production",
+  mode: 'production',
   devtool: false,
   plugins,
   optimization: {
     splitChunks: {
       cacheGroups: {
         styles: {
-          name: "styles",
-          type: "css/mini-extract",
-          chunks: "all",
+          name: 'styles',
+          type: 'css/mini-extract',
+          chunks: 'all',
           enforce: true,
         },
         vendors: {
           test: /[\\/]node_modules[\\/]/,
-          name: "vendor",
-          chunks: "all",
+          name: 'vendor',
+          chunks: 'all',
         },
       },
     },
   },
   output: {
-    filename: "assets/bundle-[name]-[chunkhash].js",
-    chunkFilename: "assets/bundle-[name]-[chunkhash].js",
-    assetModuleFilename: "assets/[name]-[hash][ext]",
+    filename: 'assets/bundle-[name]-[chunkhash].js',
+    chunkFilename: 'assets/bundle-[name]-[chunkhash].js',
+    assetModuleFilename: 'assets/[name]-[hash][ext]',
     publicPath: appConfig.baseUrl + appConfig.path,
-    path: path.resolve(process.cwd(), "build", appConfig.env),
+    path: path.resolve(process.cwd(), 'build', appConfig.env),
   },
 });
 
