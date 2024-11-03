a00703f9ed28a64a3d06b8ff223ffad0d904e90e
Danilo Rodrigues <danilo@fillet.com.br>
2024-11-03 13:38:28 -0300

    Correção no tost notification

--- app-base/src/features/toastify-notification/Container.tsx
+++ app-base/src/features/toastify-notification/Container.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 
-import 'react-toastify/dist/ReactToastify.css';
-
 const ToastNotificationContainer = (): JSX.Element => <ToastContainer position='bottom-center' />;
 
 ToastNotificationContainer.defaultProps = {};
